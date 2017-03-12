87061375a2db7d2012eb65a62be4ef74caa02e7a
Andrea Dalrymple <andrea.dalrymple@yahoo.com>
2017-03-12 09:35:55 -0400

    week 4 homework

---
+++ Week4/assets/javascript/game.js
@@ -0,0 +1,56 @@
+var wins = 0;
+var losses = 0;
+var score = 0;
+var computerChoice = Math.floor(Math.random() * 100 + 1) + 50;
+var randomNumbers = [];
+
+for (var i = 0; i < 4; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 15) + 1);
+    score++;
+}
+
+
+
+$(function() {
+    var score = 0;
+
+    //when the game starts
+    //when you load the page
+    //setup initial game
+    var computerChoice = Math.floor(Math.random() * 100 + 1) + 50;
+    var randomNumbers = [];
+
+
+    for (var i = 0; i < 4; i++) {
+        randomNumbers.push(Math.floor(Math.random() * 15) + 1);
+    }
+    console.log(randomNumbers);
+
+    $("small").html(computerChoice);
+
+
+    $("img").click(function() {
+        var imgId = $(this).attr("id");
+        console.log(imgId);
+
+        var idxFromId = parseInt(imgId.replace("crystal", "")) - 1;
+        score += randomNumbers[idxFromId];
+
+        document.querySelector("#score").innerHTML = score;
+
+        //if computerChoice is equal score 
+        if (computerChoice === score) {
+            wins++
+            document.querySelector("#wins").innerHTML = wins;
+            alert("Winner!")
+            window.location.reload()
+        } else if (score > computerChoice) {
+            losses++
+            document.querySelector("#losses").innerHTML = losses;
+            alert("Try again!")
+            window.location.reload()
+        }
+
+    });
+
+});
