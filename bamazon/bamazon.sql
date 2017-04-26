7237d62a983f6aef1160c7325edbff3d3745072c
Andrea Dalrymple <andrea.dalrymple@yahoo.com>
2017-04-26 13:10:15 -0400

    updated homework

---
+++ bamazon/bamazon.sql
@@ -0,0 +1,106 @@
+-- CREATE DATABASE bamazon_db;
+-- 
+USE bamazon_db; 
+-- 
+-- CREATE TABLE products (
+-- 	item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+-- 	product_name varchar(30) not null,
+-- 	department_name VARCHAR(50),
+-- 	price INTEGER(10), 
+-- 	stock_quantity INTEGER(10) NOT NULL,
+--     primary key(item_id)
+-- ); 
+
+insert into products(
+	product_name, 
+	department_name, 
+	price, 
+	stock_quantity 
+)
+
+select
+	"soap",
+	"toiletries",
+	2,
+	10 
+
+union all 
+
+select
+	"candy",
+	"grocery",
+	1,
+	5 
+
+union all 
+
+select
+	"books",
+	"library",
+	8,
+	7 
+
+union all 
+
+select
+	"water bottle",
+	"home",
+	5,
+	4 
+
+union all
+
+select
+	"computer",
+	"electronic",
+	100,
+	10 
+
+union all 
+
+select
+	"chair",
+	"home",
+	8,
+	15 
+
+union all 
+
+select
+	"desk",
+	"home",
+	100,
+	2 
+
+union all 
+
+select
+	"lamp",
+	"home",
+	50,
+	5 
+
+union all 
+
+select
+	"mouse",
+	"electronic",
+	14,
+	10 
+
+union all 
+
+select
+	"phone",
+	"electronics",
+	150,
+	10 
+
+
+
+
+
+
+
+
+
