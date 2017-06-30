e16cd58cb44b93ac396a23b5b507ae0165121f3f
Andrea Dalrymple <andrea.dalrymple@yahoo.com>
2017-06-29 20:07:03 -0400

    updated HW

---
+++ week10/keys.js
@@ -0,0 +1,9 @@
+console.log("Keys loaded");
+
+// API credentials by following these steps:
+exports.twitterKeys = {
+  consumer_key: "SZzsigw3iqE0pSADBUJkENCab",
+  consumer_secret: "AMw4tYFP7an0lIul49C21aS9fk2cWZTGS8XVE9bjb2JFOlNa36",
+  access_token_key: "2722037155-UARaU6pdJvvjerc1jKNuZXxprSnk4bPlbIHqY81",
+  access_token_secret: "wnpv3qmcRgT4tBLvzFEnPAqU9ih2fKkLtbE95ZIFXAaQo"
+};
